39545affd8dd387865058d07bcdaf9f6633cbdf3
SuriveAUT <84202629+SuriveAUT@users.noreply.github.com>
2023-10-06 13:10:33 +0200

    Add files via upload

---
+++ Basics_3_1.java
@@ -0,0 +1,8 @@
+public class Basics_3_1 {
+    public static void main(String[] args) {
+        long n = 1_000_000_000; // eine Milliarde
+        System.out.println("Eine Milliarden: " + n);
+        System.out.println("Zwei Milliarden: " + (n + n));
+        System.out.println("Drei Milliarden: " + (n + n + n));
+    }
+}
